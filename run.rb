9895b4bd419f06ce22e21c6ae291746999e04427
pacafs <paca.fs@gmail.com>
2016-06-12 21:46:57 +0100

    first commit

---
+++ run.rb
@@ -0,0 +1,17 @@
+require 'json'
+require 'roo'
+
+file = File.open("code.json", "r") 
+data = file.read 
+hash = JSON.parse(data)
+
+xlsx = Roo::Spreadsheet.open('similar.xlsx')
+xlsx.sheet('test').each do |x|
+	hash["startUrl"].push("https://pro.similarweb.com/#/website/audience-overview/" + x[0] + "/*/999/3m?webSource=Total")
+end
+
+puts hash["startUrl"]
+puts "########"
+puts "########"
+puts hash.to_json
+hash = []
